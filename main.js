20c698871f46a1710147bf0cf77a653b40a464b6
unknown <ismael.vidal.b@gmail.com>
2024-02-21 13:24:46 -0300

    cambio mayusculas

--- main.js
+++ main.js
@@ -4,12 +4,19 @@ function validateForm () {
   //1.- Me traigo los valores del formulario
   const name = $('#name').val();
 
-  //2. Valido el largo del nombre
+  //2. obtengo las letras mayusculas
+
+  const primera_name = name[0];
+
+
+  //3. Valido el largo del nombre
 
   if (name.length < 3 || name.length > 8) {
     alert("Los nombres deben tener entre 3 y ocho caracteres");
-}
-else{
+  } else if(primera_name.toUpperCase() != primera_name){
+    console.log("la primera tiene que se mayuscula")
+  } else{
     alert("Todo correcto");
-}
+  }
+
 }
